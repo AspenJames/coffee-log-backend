e431d6db24dbd9f0750f40ab08f5ef16291a5a68
Aspen James <aspenjames@tqca.org>
2020-02-27 13:53:26 -0800

    Passing roaster spec

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,3 +1,13 @@
 class Roaster < ApplicationRecord
   has_many :coffees
+
+  validates :name, presence: true
+
+  def name=(*args)
+    if self.name != nil
+      raise Exceptions::ImmutableAttributeError
+    else
+      super
+    end
+  end
 end
