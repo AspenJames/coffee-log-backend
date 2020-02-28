1b8cfc8ea2fdf9f903ae495c88691f88057e04a1
Aspen James <aspenjames@tqca.org>
2020-02-28 02:13:08 -0800

    Add Coffee specs + pass

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,4 +1,10 @@
 class Coffee < ApplicationRecord
   belongs_to :roaster
   has_many :recipes
+  has_many :entries, :through => :recipes
+
+  validates :origin, :roast_date, presence: true
+  make_immutable(:origin)
+  make_immutable(:roast_date)
+
 end
--- spec/models/coffee_spec.rb
+++ spec/models/coffee_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Coffee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Attributes:" do
+    describe "origin" do
+      it_behaves_like "a required attribute" do
+        let(:attribute_name) { :origin }
+      end
+
+      it_behaves_like "an immutable attribute" do
+        let(:attribute_name) { :origin }
+      end
+    end
+
+    describe "roast_date" do
+      it_behaves_like "a required attribute" do
+        let(:attribute_name) { :roast_date }
+      end
+
+      it_behaves_like "an immutable attribute" do
+        let(:attribute_name) { :roast_date }
+      end
+    end
+
+    describe "price" do
+      it_behaves_like "an optional attribute" do
+        let(:attribute_name) { :price }
+        let(:update_value) { 3500 }
+      end
+    end
+  end
+
+  context "Relationships:" do
+    describe "Roaster:" do
+      it_behaves_like "a belongs_to relationship" do
+        let(:described) { create(:coffee) }
+        let(:relation_name) { :roaster }
+      end
+    end
+
+    describe "Recipes:" do
+      it_behaves_like "a has_many relationship" do
+        let(:described) { create(:coffee) }
+        let(:relation_name) { :recipes }
+      end
+    end
+
+    describe "Entries:" do
+      it_behaves_like "a has_many, through relationship" do
+        let(:described) { create(:coffee) }
+        let(:relation_name) { :entries }
+        let(:through_name) { :recipes }
+      end
+    end
+  end
 end
