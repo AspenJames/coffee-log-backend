3737f19694cc03c32dc5b70d9a94fd0899b972ff
Aspen James <aspenjames@tqca.org>
2020-02-28 13:03:06 -0800

    Add entry spec + pass

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ApplicationRecord
   has_one :recipe
   has_one :coffee, :through => :recipe
+
+  validates :date, :description, :rating, presence: true
+  make_immutable(:date)
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Attributes:" do
+    describe "date" do
+      it_behaves_like "a required attribute" do
+        let(:attribute_name) { :date }
+      end
+
+      it_behaves_like "an immutable attribute" do
+        let(:attribute_name) { :date }
+      end
+    end
+
+    describe "prep_notes" do
+      it_behaves_like "an optional attribute" do
+        let(:attribute_name) { :prep_notes }
+      end
+
+      it_behaves_like "a mutable attribute" do
+        let(:attribute_name) { :prep_notes }
+      end
+    end
+
+    describe "description" do
+      it_behaves_like "a required attribute" do
+        let(:attribute_name) { :description }
+      end
+
+      it_behaves_like "a mutable attribute" do
+        let(:attribute_name) { :description }
+      end
+    end
+
+    describe "rating" do
+      it_behaves_like "a required attribute" do
+        let(:attribute_name) { :rating }
+      end
+
+      it_behaves_like "a mutable attribute" do
+        let(:attribute_name) { :rating }
+        let(:update_value) { 2 }
+      end
+    end
+  end
+
+  context "Relationships:" do
+    describe "Recipe" do
+      it_behaves_like "a has_one relationship" do
+        let(:described) { create(:entry) }
+        let(:relation_name) { :recipe }
+      end
+    end
+
+    describe "Coffee" do
+    end
+  end
 end
