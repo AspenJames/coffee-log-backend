285ad33859882c56121075f312b805c3cfaeafdc
Aspen James <aspenjames@tqca.org>
2020-02-27 18:31:57 -0800

    Autoload lib/
    Ensures that custom exceptions defined in module Exceptions are loaded

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module CoffeeLogBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths += Dir["#{Rails.root}/lib/**/"]
   end
 end
