1659d6997094171523fc37fe655c022ae701cd14
Aspen James <aspenjames@tqca.org>
2020-03-05 20:08:59 -0800

    Update shared examples for attributes

--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -1,11 +1,11 @@
 RSpec.shared_examples "a required attribute" do
+  ##
+  # Describes an attribute required for model instance creation
   # attribute_name => symbol representing the required attribute
-  before(:all) do
-    raise AttributeNameRequired if !respond_to?(:attribute_name)
-  end
 
-  def described_class_sym
-    described_class.name.downcase.to_sym
+  before(:all) do
+    # ensure required value is set
+    raise AttributeNameRequired if !respond_to?(:attribute_name)
   end
 
   it "is invalid without attribute supplied" do
@@ -13,19 +13,20 @@ RSpec.shared_examples "a required attribute" do
     update_obj = {}
     update_obj[attribute_name] = nil
     attrs.merge!(update_obj)
-    expect{create(described_class_sym, attrs)}.to raise_exception(ActiveRecord::RecordInvalid)
+    expect{
+      create(described_class_sym, attrs)
+    }.to raise_exception(ActiveRecord::RecordInvalid)
   end
 end
 
 RSpec.shared_examples "an optional attribute" do
+  ##
+  # Describes an attribute that may be omitted from model creation
   # attribute_name => symbol representing the optional attribute
-  # update_value   => optional value to test mutability
-  before(:all) do
-    raise AttributeNameRequired if !respond_to?(:attribute_name)
-  end
 
-  def described_class_sym
-    described_class.name.downcase.to_sym
+  before(:all) do
+    # ensure required value is set
+    raise AttributeNameRequired if !respond_to?(:attribute_name)
   end
 
   it "can be omitted from the creation args" do
@@ -39,19 +40,21 @@ RSpec.shared_examples "an optional attribute" do
 end
 
 RSpec.shared_examples "an immutable attribute" do
+  ##
+  # Describes an attribute that cannot be changed once set
   # attribute_name => symbol representing the immutable attribute
-  before(:all) do
-    raise AttributeNameRequired if !respond_to?(:attribute_name)
-  end
 
-  def described_class_sym
-    described_class.name.downcase.to_sym
+  before(:all) do
+    # ensure required value is set
+    raise AttributeNameRequired if !respond_to?(:attribute_name)
   end
 
   it "cannot be updated once set" do
     described = create(described_class_sym)
     orig_val = described.send(attribute_name)
     update_arg = {}
+    # The type of value for update_arg does not matter
+    # since we are expecting an exception to be thrown
     update_arg[attribute_name] = ""
     expect{described.update(update_arg)}.to raise_exception(Exceptions::ImmutableAttributeError)
     expect(described.send(attribute_name)).to eq orig_val
@@ -59,29 +62,30 @@ RSpec.shared_examples "an immutable attribute" do
 end
 
 RSpec.shared_examples "a mutable attribute" do
+  ##
+  # Describes an attribute that may be updated
   # attribute_name => symbol representing the mutable attribute
-  # update_value => value to update, default to "New value"
-  before(:all) do
-    raise AttributeNameRequired if !respond_to?(:attribute_name)
-  end
+  # update_value => value to update; optional, defaults to "UpdateValue"
 
-  def described_class_sym
-    described_class.name.downcase.to_sym
+  before(:all) do
+    # ensure required value is set
+    raise AttributeNameRequired if !respond_to?(:attribute_name)
   end
 
   it "can be updated once set" do
     described = create(described_class_sym)
     orig_val = described.send(attribute_name)
 
-    update_value ||= "UpdateValue"
+    update_val = respond_to?(:update_value) ? update_value : "UpdateValue"
 
     update_arg = {}
-    update_arg[attribute_name] = update_value
+    update_arg[attribute_name] = update_val
     described.update(update_arg)
     expect(described.valid?).to be true
 
     new_val = described.send(attribute_name)
     expect(orig_val).not_to eq new_val
+    expect(new_val).to eq update_val
   end
 end
 
@@ -92,7 +96,13 @@ class AttributeNameRequired < StandardError
     => ex. class Blog; validates :title, presence: true; end
     => let(:attribute_name) { :title }
   TEXT
-  def initialize(msg=DEFAULT_MSG)
-    super(msg)
-  end
+  def initialize(msg=DEFAULT_MSG); super(msg); end
+end
+
+##
+# Returns a symbol representing the described class
+# e.g. within RSpec.describe Coffee:
+# described_class_sym #=> :coffee
+def described_class_sym
+  described_class.name.downcase.to_sym
 end
